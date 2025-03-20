a5fb39b565695f6ebfce4a1f34d42da174b66261
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2025-03-20 18:53:20 +0530

    updates Home pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
 import HappySection from "../components/HappySection";
@@ -9,62 +10,80 @@ import HowItWorks from "./HowItWork";
 import Partners from "./Partners";
 import PartnerBusinessProgram from "./PartnerBusinessProgram";
 
-
-
-
-
+// ✅ Importing images correctly
+import image1 from "../assets/images/1.png";
+import image2 from "../assets/images/2.png";
+import image3 from "../assets/images/3.png";
+import image4 from "../assets/images/4.png";
 
 const Home = () => {
   return (
     <>
       <Carousel>
         <Carousel.Item>
-          <img src="./src/assets/images/4.png" width="100%" height="470vh" className="caro" />
+          <img src={image4} width="100%" height="470" className="caro" alt="Slide 1" />
           <Carousel.Caption>
-            
-              <Button style={{marginRight:"100%", width:"25%", borderRadius:"20px"  }} variant="primary"onClick={() => { window.location.href = "/applynow" }}>Apply Now</Button>
-          
+            <Button
+              style={{ marginRight: "100%", width: "25%", borderRadius: "20px" }}
+              variant="primary"
+              onClick={() => { window.location.href = "/applynow"; }}
+            >
+              Apply Now
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img src="./src/assets/images/3.png" width="100%" height="470" className="caro" />
-          <Carousel.Caption>
-            
-              <Button style={{marginRight:"100%", width:"25%", borderRadius:"20px" }} variant="primary"onClick={() => { window.location.href = "/applynow" }}>Apply Now</Button>
 
-            
+        <Carousel.Item>
+          <img src={image3} width="100%" height="470" className="caro" alt="Slide 2" />
+          <Carousel.Caption>
+            <Button
+              style={{ marginRight: "100%", width: "25%", borderRadius: "20px" }}
+              variant="primary"
+              onClick={() => { window.location.href = "/applynow"; }}
+            >
+              Apply Now
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img src="./src/assets/images/1.png" width="100%" height="470" className="caro" />
-          <Carousel.Caption>
-            
-          <Button style={{marginRight:"100%", width:"25%", borderRadius:"20px" }} variant="primary"onClick={() => { window.location.href = "/applynow" }}>Apply Now</Button>
 
-            
+        <Carousel.Item>
+          <img src={image1} width="100%" height="470" className="caro" alt="Slide 3" />
+          <Carousel.Caption>
+            <Button
+              style={{ marginRight: "100%", width: "25%", borderRadius: "20px" }}
+              variant="primary"
+              onClick={() => { window.location.href = "/applynow"; }}
+            >
+              Apply Now
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-          <img src="./src/assets/images/2.png" width="100%" height="470" className="caro" />
-          <Carousel.Caption>
-          
-          <Button style={{marginRight:"100%", width:"25%", borderRadius:"20px" }} variant="primary"onClick={() => { window.location.href = "/applynow" }}>Apply Now</Button>
 
-            
+        <Carousel.Item>
+          <img src={image2} width="100%" height="470" className="caro" alt="Slide 4" />
+          <Carousel.Caption>
+            <Button
+              style={{ marginRight: "100%", width: "25%", borderRadius: "20px" }}
+              variant="primary"
+              onClick={() => { window.location.href = "/applynow"; }}
+            >
+              Apply Now
+            </Button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
-      <HappySection/>
-      <FeaturesSection/>
+
+      {/* Other sections */}
+      <HappySection />
+      <FeaturesSection />
       <BankOffers />
       <TalkToUs />
-      
-      <CalculatorSection/>
+      <CalculatorSection />
       <HowItWorks />
-      <Partners/>
-      <PartnerBusinessProgram/>
+      <Partners />
+      <PartnerBusinessProgram />
     </>
-  )
-}
+  );
+};
 
 export default Home;
